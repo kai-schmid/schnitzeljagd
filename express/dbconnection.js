1aee8ec06e24750e7de23c554949bb34d9f592b8
kai-schmid <kai.schmid@live.de>
2023-10-04 17:40:55 +0200

    cahange dbconnection

--- express/dbconnection.js
+++ express/dbconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/users', {
+mongoose.connect('mongodb://172.20.0.2:27017/users', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
